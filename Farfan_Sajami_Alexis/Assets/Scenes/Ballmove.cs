76f774acc1a60afa2291dfbffef846df8008f39a
AlexisFarfan <jumpytunechi@gmail.com>
2020-11-30 18:02:28 +0100

    final

---
+++ Farfan_Sajami_Alexis/Assets/Scenes/Ballmove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ballmove : MonoBehaviour
+{
+    public GameObject myPrefab;
+    public Transform referenceObject;
+    public Input axes;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
